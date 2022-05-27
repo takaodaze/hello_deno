ea4d8c32962d031c97dc7ae719008d53808a1ae6
takaodaze <gin2798@gmail.com>
2022-05-28 00:47:07 +0900

    create touch command

--- touch/main.ts
+++ touch/main.ts
@@ -1,8 +1,12 @@
 import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
 
-await new Command()
+const { args, options, cmd, literal } = await new Command()
   .name("touch")
   .version("0.1.0")
-  .usage("file")
+  .arguments("<file>")
+  .option("-p --path <path:string>", "path option")
   .description("touch command")
   .parse(Deno.args);
+
+const file = args[0];
+Deno.createSync(`${file}`);
